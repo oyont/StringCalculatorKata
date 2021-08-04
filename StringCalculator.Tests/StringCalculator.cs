a86dede4d600e48ed9ceff375fdfe879494d7ea1
Onur Yönt <onur.yont@gmail.com>
2021-08-04 09:36:44 +0300

    step 2

--- StringCalculator.Tests/StringCalculator.cs
+++ StringCalculator.Tests/StringCalculator.cs
@@ -1,6 +1,28 @@
+using System;
+using System.Linq;
+
 namespace StringCalculator.Tests
 {
     public class StringCalculator
     {
+        public int Add(string numbers)
+        {
+            if (string.IsNullOrWhiteSpace(numbers))
+                return 0;
+
+            try
+            {
+                var sum = numbers
+                 .Split(',')
+                 .Select(int.Parse)
+                 .Sum();
+
+                return sum;
+            }
+            catch (System.Exception ex)
+            { 
+                throw new ArgumentException("Unknown number");
+            }
+        }
     }
 }
--- StringCalculator.Tests/StringCalculatorTest.cs
+++ StringCalculator.Tests/StringCalculatorTest.cs
@@ -1,3 +1,5 @@
+using System;
+using FluentAssertions;
 using NUnit.Framework;
 
 namespace StringCalculator.Tests
@@ -13,9 +15,38 @@ namespace StringCalculator.Tests
         }
 
         [Test]
-        public void Test1()
+        public void Add_Returns_0_When_Given_Empty_String()
         {
-            Assert.Pass();
+            var result = _sut.Add("");
+
+            result.Should().Be(0);
+        }
+
+        [TestCase("1",1)]
+        [TestCase("2",2)]
+        public void Add_Returns_Sum_When_Given_1_Number(string numbers, int expected)
+        {
+            var result = _sut.Add(numbers);
+
+            result.Should().Be(expected);
+        }
+
+        [TestCase("1,2", 3)]
+        [TestCase("2,3", 5)]
+        public void Add_Returns_Sum_When_Given_2_Numbers(string numbers, int expected)
+        {
+            var result = _sut.Add(numbers);
+
+            result.Should().Be(expected);
+        }
+
+        [Test] 
+        public void Add_Should_Handle_Unknown_Number()
+        {
+            Action action = () => { _sut.Add("a"); };
+
+            action.Should().Throw<ArgumentException>() 
+                .WithMessage("Unknown number");
         }
     }
 }
