b5858fcb33f7a1c69d8bc6a34b5ae7e4a8eb3b37
Onur Yönt <onur.yont@gmail.com>
2021-08-04 12:48:55 +0300

    refactor

--- StringCalculator.Tests/StringCalculator.cs
+++ StringCalculator.Tests/StringCalculator.cs
@@ -7,39 +7,55 @@ namespace StringCalculator.Tests
 {
     public class StringCalculator
     {
+        private List<string> _delimiters = new List<string> { ",", "\n" };
+        private const string CUSTOM_DELIMITER_INDICATOR = "//";
+
         public int Add(string numbers)
         {
             if (string.IsNullOrWhiteSpace(numbers))
                 return 0;
-
-            var delimiters = new List<string> { ",", "\n" };
              
-            if (numbers.StartsWith("//"))
+            if (numbers.StartsWith(CUSTOM_DELIMITER_INDICATOR))
             {
-                var delimiter = numbers.Split('\n').First().Replace("//", "");
-
-                var customDelimiters = delimiter.Split("][",StringSplitOptions.RemoveEmptyEntries).Select(d=> d.Replace("[","").Replace("]",""));
-                 
-                 
-                delimiters.AddRange(customDelimiters); 
-                numbers = numbers.Split('\n', 2).Last();
+                numbers = AdCustomDelimiter(numbers);
             }
 
-            var numbersToSum = numbers
-             .Split(delimiters.ToArray(), StringSplitOptions.None)
-             .Select(int.Parse)
-             .Where(x => x <= 1000);
+            var numbersToSum = ParseNumbersToSum(numbers);
 
+            CheckNegativeNumbers(numbersToSum);
+             
+            return numbersToSum.Sum();
+
+        }
+
+        private static void CheckNegativeNumbers(List<int> numbersToSum)
+        {
             var negatives = numbersToSum.Where(x => x < 0).ToList();
 
             if (negatives.Any())
             {
                 throw new Exception($"Negatives not allowed: {string.Join(',', negatives)}");
             }
+        }
 
+        private List<int> ParseNumbersToSum(string numbers)
+        { 
+            return numbers
+                .Split(_delimiters.ToArray(), StringSplitOptions.None)
+                .Select(int.Parse)
+                .Where(x => x <= 1000).ToList();
+        }
 
-            return numbersToSum.Sum();
+        private string AdCustomDelimiter(string numbers)
+        { 
+            var delimiter = numbers.Split('\n').First().Replace(CUSTOM_DELIMITER_INDICATOR, "");
 
+            var customDelimiters = delimiter.Split("][", StringSplitOptions.RemoveEmptyEntries)
+                .Select(d => d.Replace("[", "").Replace("]", ""));
+             
+            _delimiters.AddRange(customDelimiters);
+            numbers = numbers.Split('\n', 2).Last();
+            return numbers;
         }
     }
 }
